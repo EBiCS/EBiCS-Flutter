9a297cc1032704350fa01391e7e3c363f93b3a5d
stancecoke <hochsitzcola@web.de>
2020-11-08 11:51:57 +0100

    assets for icon logo added

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     theme: ThemeData(
       primarySwatch: Colors.lightBlue,
     ),
-    home: MyHomePage(title: 'Control Center'),
+    home: MyHomePage(title: 'v.01'),
 
   );
 }
